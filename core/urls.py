bdae3a2d7b715ab913a3acea2376a06c5f6654ae
Jacob Costa <jacobcosta.costasilva@gmail.com>
2023-04-05 20:52:11 -0400

    Atualizações

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
-from .views import ListarPostsListView
+from django.urls import path
+from .views import ListarPosts, IndexView
 
 urlpatterns = [
-    path('', ListarPostsListView.as_view(), name='home'),
+    path('', IndexView.as_view(), name='home'),
+    path('post/<slug:slug>', ListarPosts.as_view(), name='post'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 
 from core.models import Post
 
 
-class ListarPostsListView(ListView):
+class IndexView(ListView):
     context_object_name = 'posts'
-    template_name = 'blog/post/listarposts.html'
+    template_name = "blog/index.html"
     queryset = Post.publicados.all()
+    paginate_by = 2
+
+class ListarPosts(TemplateView):
+
+    def get_context_data(self, **kwargs):
+        context = super(ListarPosts, self).get_context_data(**kwargs)
+        queryset = Post.publicados.get(slug=kwargs['slug'])
+        context.update({'item':queryset})
+        return context
+
+    context_object_name = 'item'
+    template_name = "blog/post/detalheposts.html"
